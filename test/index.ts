8878a45fda190628028819350e1e8addbcdd36ab
StoneT2000 <stoneztao@gmail.com>
2020-05-21 10:05:05 -0700

    1.3.4

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,6 @@
 import HideAndSeekDesign from '../src';
 import { create, Logger } from 'dimensions-ai';
+import { Dimension } from 'dimensions-ai/lib/Dimension';
 
 let hideandseekdesign = new HideAndSeekDesign('HideAndSeek!', {
   engineOptions: {
@@ -14,26 +15,26 @@ let hideandseek = create(hideandseekdesign, {
   loggingLevel: Logger.LEVEL.WARN,
   activateStation: true,
   observe: true,
-  secureMode: true
 });
 
 let randomBot = {file: './kits/js/bot.js', name: 'random'};
 let stillbot = {file: './test/bots/js/stillbot/bot.js', name:'still'};
 let chaseBot = {file: './test/bots/js/chasebot/bot.js', name: 'chaser'};
-let botlist = [chaseBot, randomBot];
+let badBot = {file: './test/bots/js/badbot/bot.js', name: 'badbot'};
+let botlist = [chaseBot, stillbot];
 // for (let i = 0; i < 2; i++) {
 
 //   botlist.push({file: './kits/js/bot.js', name: 'random_' + i});
 // }
 let bot = {file: './kits/js/bot.js', name: 'bot'};
+// seed 30 is bad
 hideandseek.runMatch(botlist, {
   delay: 0.2,
-  randomizeSeeker: true,
-  liveView: false,
-  seed: 51131
+  liveView: true,
 }).then((res) => {
   console.log(res);
 });
+// let n = 'match_h1zXUtXiBagA.json'
+// HideAndSeekDesign.watch('./replays/' + n, 0.1);
 
-HideAndSeekDesign.watch('./replays/match_0gd7DHGLXUzL.json', 0.1);
-
+// let app = hideandseek.getStation().app;
