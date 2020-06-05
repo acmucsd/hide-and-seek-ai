8ec0d255b691f741a46e799d54744fec4fb022bd
StoneT2000 <stoneztao@gmail.com>
2020-06-05 14:44:39 -0700

    Update setup.sh

--- server/setup.sh
+++ server/setup.sh
@@ -3,8 +3,8 @@ apt-get update
 
 # install core files needed by server
 npm install pm2 -g
-npm install @acmucsd/hide-and-seek-ai
-npm install dimensions-ai
+npm install @acmucsd/hide-and-seek-ai@1.9.8
+npm install dimensions-ai@3.1.2
 
 # setup node related configs
 pm2 install pm2-logrotate
