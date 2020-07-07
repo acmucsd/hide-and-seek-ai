7e22fc38bb6119265b009f60d952e7a8a73776b4
StoneT2000 <stonezt2019@gmail.com>
2020-07-07 15:34:59 -0700

    fix matchmaking possibly

--- server/run.prod.js
+++ server/run.prod.js
@@ -26,7 +26,6 @@ let hideandseek = Dimension.create(hideandseekdesign, {
     disableUploads: true
   }
 });
-
 let mongo = new MongoDB(process.env.MONGO_STRING);
 let gcloudstorage = new GCloudStorage({
   projectId: "proto-code",
@@ -43,7 +42,7 @@ Promise.all(promises).then(() => {
     name: 'Hide and Seek',
     consoleDisplay: false,
     defaultMatchConfigs: {
-      randomizeSeeker: true,
+      randomizeSeeker: false,
       liveView: false,
       loggingLevel: Dimension.Logger.LEVEL.ERROR,
       storeReplayDirectory: 'hide_and_seek_official_tournament/replays',
@@ -55,6 +54,35 @@ Promise.all(promises).then(() => {
     agentsPerMatch: [2],
     tournamentConfigs: {
       maxConcurrentMatches: 1,
+      matchMake: (rankings) => {
+        let sortedPlayers = rankings.map((p) => p.player).filter((p) => !p.disabled);
+        let newQueue = [];
+        sortedPlayers.forEach((player, rank) => {
+
+          
+          // take random competitors from +/- competitorCount * 2.5 ranks near you
+          if (sortedPlayers.length < 2) {
+            return;
+          }
+          let oplayer = sortedPlayers[rank + 1];
+          if (rank == sortedPlayers.length - 1) {
+            oplayer = sortedPlayers[rank - 1];
+          }
+          let lower = 0
+          let upper = 6
+          lower = Math.max(0, rank - 5);
+          upper = Math.min(sortedPlayers.length, rank + 5);
+
+          let selectFrom = [...sortedPlayers.slice(lower, rank), ...sortedPlayers.slice(rank + 1, upper)];
+          oplayer = selectFrom[Math.floor(Math.random() * selectFrom.length)];
+          newQueue.push([player, oplayer], [oplayer, player]);
+        });
+
+        // console.log(newQueue.map((a) => {
+        //   return [a[0].tournamentID.name, a[1].tournamentID.name]
+        // }));
+        return newQueue;
+      }
     },
     resultHandler: HideAndSeekDesign.trueskillResultHandler,
     id: 'a0Zlpa'
@@ -72,3 +100,13 @@ sudo cgcreate -g memory,cpu:botgroup
 sudo cgset -r memory.limit_in_bytes=50M botgroup
 sudo cgexec -g memory,cpu:botgroup sudo node run.prod.js
 */
+
+const shuffle =(arr) => {
+  for (let i = arr.length - 1; i >= 1; i--) {
+    let j = Math.floor(Math.random() * i);
+    let tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+  return arr;
+}
