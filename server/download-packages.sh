da0086cb9e45353f6ed7b6454ab49fc7c1fc8792
StoneT2000 <stonezt2019@gmail.com>
2020-07-07 01:57:52 -0700

    update

--- server/download-packages.sh
+++ server/download-packages.sh
@@ -1,2 +1,6 @@
 # install python packages
 sudo -H -u dimensions_bot pip3 install --user numpy scipy
+
+# install engine
+npm install @acmucsd/hide-and-seek-ai@1.9.14
+npm install dimensions-ai@3.2.3
